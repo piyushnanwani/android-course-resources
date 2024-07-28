2d4cc5f01239ec6bf4ff21ef68bf6fd1f8dd7843
Piyush Nanwani <pnanwani61@gmail.com>
2024-07-28 20:44:01 +0530

    Logic of Todo list screen

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -9,17 +9,33 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
+//    private val itemList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnAdd.setOnClickListener {
+           // 1. First get the text of the text input
+            val text = binding.etNewItem.text.toString()
+            if (text.isNotEmpty()) {
+                binding.etNewItem.text.clear() // clear the text input
+                if (binding.textView.text.equals("No items present")) {
+                    binding.textView.text = ""
+                }
+                    binding.textView.text = binding.textView.text.toString() + "\n" + text
+
+            }
         }
 
     }
